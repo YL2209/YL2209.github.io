2ce5e0e87b972d8a0b2398ca69a99b9f8e5be239
YL2209 <3021027861@qq.com>
2024-02-10 01:39:57 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -1,22 +1,20 @@
 try {
-  const Theme = saveToLocal.get("theme")
-  if (Theme === "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else if (Theme === "light") {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
   // 昼夜切换
   function darkNuuton() {
     const BUTTON = document.querySelector(".components");
     const TOGGLE = () => {
       const willChangeMode =
         document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"; // 判断是否白天黑夜;
-      document.documentElement.setAttribute("data-theme", willChangeMode);
+      if (willChangeMode === "dark") {
+        activateDarkMode();
+      } else {
+        activateLightMode();
+      }
       saveToLocal.set("theme", willChangeMode, 2);
     };
     BUTTON.addEventListener("click", TOGGLE);
   }
-  darkNuuton()
+  darkNuuton();
 } catch (error) {
   console.log(error)
 }
