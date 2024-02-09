b9d692dca92bfdc5a9629c32d0f85e40e4a8bca0
YL2209 <3021027861@qq.com>
2024-02-09 22:56:34 +0800

    npm publish

---
+++ js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", (function () {
+  const BUTTON = document.querySelector(".components");
+  const TOGGLE = () => {
+    const willChangeMode =
+      document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"; // 判断是否白天黑夜;
+    document.documentElement.setAttribute("data-theme", willChangeMode);
+    saveToLocal.set("theme", willChangeMode, 2);
+  };
+  if (saveToLocal.get("theme") === "dark") {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+  BUTTON.addEventListener("click", TOGGLE);
+}));
