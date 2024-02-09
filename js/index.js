c76741169e0f53ae2b16fc4eec0ab309146206e7
YL2209 <3021027861@qq.com>
2024-02-09 23:04:05 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded", (function () {
-  const BUTTON = document.querySelector(".components");
-  const TOGGLE = () => {
-    const willChangeMode =
-      document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"; // 判断是否白天黑夜;
-    document.documentElement.setAttribute("data-theme", willChangeMode);
-    saveToLocal.set("theme", willChangeMode, 2);
-  };
-  BUTTON.addEventListener("click", TOGGLE);
-  
+try {
   if (saveToLocal.get("theme") === "dark") {
     document.documentElement.setAttribute("data-theme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
   }
-}));
+  function darkNuuton() {
+    const BUTTON = document.querySelector(".components");
+    const TOGGLE = () => {
+      const willChangeMode =
+        document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"; // 判断是否白天黑夜;
+      document.documentElement.setAttribute("data-theme", willChangeMode);
+      saveToLocal.set("theme", willChangeMode, 2);
+    };
+    BUTTON.addEventListener("click", TOGGLE);
+  }
+  darkNuuton()
+} catch (error) {
+console.log(error)
+}
+
